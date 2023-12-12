957fdb52eebe3be823ca34b37e948852e7014644
ikramulhasan1 <hasanikramul926@gmail.com>
2023-12-12 14:10:46 +0600

    category create

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\admin\AdminLoginController;
-use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\AdminLoginController;
 
 
 Route::get('/', function () {
@@ -19,5 +20,10 @@ Route::group(['prefix' => 'admin'], function () {
     Route::group(['middleware' => 'admin.auth'], function () {
         Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
         Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+
+        // Category Routes
+        Route::resources([
+            'categories' => CategoryController::class,
+        ]);
     });
 });
